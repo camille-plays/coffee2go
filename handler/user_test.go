af2b8cf5431c0beec2f17dfe612a65abbb2c1203
Camille Plays <camille.plays@wise.com>
2022-12-02 15:42:36 +0100

    add user tests

--- handler/user_test.go
+++ handler/user_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -26,4 +27,59 @@ func TestGetUsers(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, http.StatusOK, w.Code)
 	require.Equal(t, users, h.DB.GetUsers())
+}
+
+func TestCreateUser(t *testing.T) {
+	router := gin.Default()
+	h := NewTestHandler()
+	router.POST("/user", h.CreateUser)
+
+	jsonBody := []byte(`{"name": "Foo", "email": "Foo@gmail.com"}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	req, _ := http.NewRequest("POST", "/user", bodyReader)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	var newUser dao.User
+
+	err := json.NewDecoder(w.Body).Decode(&newUser)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusCreated, w.Code)
+	require.Equal(t, newUser.Name, "Foo")
+	require.Equal(t, newUser.Email, "Foo@gmail.com")
+}
+
+func TestCreateUserInvalidRequest(t *testing.T) {
+	router := gin.Default()
+	h := NewTestHandler()
+	router.POST("/user", h.CreateUser)
+
+	jsonBody := []byte(`{"name": "Foo"}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	req, _ := http.NewRequest("POST", "/user", bodyReader)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestGetUserById(t *testing.T) {
+	router := gin.Default()
+	h := NewTestHandler()
+	router.GET("/user/:id", h.GetUserByID)
+
+	validId := h.DB.GetUsers()[0].ID
+
+	req, _ := http.NewRequest("GET", "/user/"+validId, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	var firstUser dao.User
+
+	err := json.NewDecoder(w.Body).Decode(&firstUser)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, w.Code)
+	require.Equal(t, firstUser.ID, validId)
 }
